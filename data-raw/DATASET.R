effcb40456272e9bff452caaaf2be69358348b69
anusha-zaman <anushazaman@college.harvard.edu>
2023-05-06 23:34:09 -0400

    added wrangled dataset with packages

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,10 +2,10 @@
 
 ## NOT DONE: Will create clean csvs, put in data folder, & update DATASET.R
 us_states <- tigris::states(cb = TRUE, resolution = "20m", progress_bar = FALSE)
-us_states_shifted2 <- tigris::shift_geometry(us_states,
+us_states_shifted <- tigris::shift_geometry(us_states,
                                              preserve_area = TRUE,
-                                             position = "below")
-states <- left_join(us_states_shifted2, USStates, by = c("NAME" = "State"))
+                                             position = "outside")
+states <- inner_join(us_states_shifted, Lock5Data::USStates, by = c("NAME" = "State"))
 
 
 crash_data <- readr::read_csv("https://raw.githubusercontent.com/harvard-stat108s23/materials/main/psets/data/cambridge_cyclist_ped_crash.csv")
