83f6697ca60a499bf4f54fd70e97bc41ed8c8a07
anusha-zaman <anushazaman@college.harvard.edu>
2023-05-10 08:19:30 -0400

    updating dataset documentation with classes; formatting

--- R/states.R
+++ R/states.R
@@ -1,13 +1,24 @@
-#' @format This package contains two data sets. The first contains information
-#'         on the residents of US states using a combination of two other datasets
-#'         found in the Lock5Data & tigris packages:
-#'            NAME: Name of state
-#'            STUSPS: State abbreviation
-#'            Region: MW=Midwest, NE=Northeast, S=South, W=West
-#'            geometry: Shape data for the state
-#'            Elect2016: State winner in 2016 presidential election (D=Clinton, R=Trump)
-#'            Population: Number of residents (in millions for 2014)
-#'            Vegetables: Percentage of residents eating vegetables at least once per day
+#' Dataset 2 of 2 used in this package to test and demo functions.
+#'
+#' This package contains two data sets. The first contains information
+#' on the residents of US states using a combination of two other datasets
+#' found in the Lock5Data & tigris packages.
+#'
+#' @format A simple-feature dataframe with 50 rows and 7 variables:
+#' \describe{
+#'  \item{NAME}{a character string denoting name of the state}
+#'  \item{STUSPS}{a character string denoting postal abbreviation for the state}
+#'  \item{Region}{a factor denoting geographical region of the state
+#'  (MW=Midwest, NE=Northeast, S=South, W=West)}
+#'  \item{geometry}{Multipolygon shapefile data for the state}
+#'  \item{Elect2016}{a factor denoting state winner in the 2016 presidential
+#'  election (D=Clinton, R=Trump)}
+#'  \item{Population}{a number denoting the number of residents in 2014
+#'  (in millions)}
+#'  \item{Vegetables}{a number denoting percentage of residents eating
+#'  vegetables at least once per day (in %)}
+#' }
+#'
 #' @source states (Dataset 1): https://cran.r-project.org/web/packages/Lock5Data/index.html
 #' @source states (Dataset 2): https://github.com/walkerke/tigris/blob/master/R/states.R
 "states"
