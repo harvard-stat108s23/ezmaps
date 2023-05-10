0b0bca8eab6c0fc767539ec44c854967df3d31e1
anusha-zaman <anushazaman@college.harvard.edu>
2023-05-10 08:57:49 -0400

    removed fill variable

--- R/tessellate.R
+++ R/tessellate.R
@@ -1,7 +1,6 @@
 #' Fits hexagon tessellations to geographical multipolygon data as base for hexbin maps
 #'
 #' @param data Dataset with geometry/multipolygon data
-#' @param fill Variable used to fill the hexagons
 #' @param geometry Variable that gives geometry/multipolygon data
 #' @param n Number of seed maps to generate; default 6
 #' @param ...
@@ -10,18 +9,14 @@
 #' @export
 #'
 #' @examples
-#' tessellate(data = states, fill = "Vegetables", geometry = geometry, n = 6)
+#' tessellate(data = states, geometry = geometry, n = 6)
 
-tessellate <- function(data, fill, geometry, n = 6,...) {
+tessellate <- function(data, geometry, n = 6,...) {
 
   # Check inputs
   ## Check: dataset contains geometry
   if (!inherits(data, "sf")) {
     stop("data does not include a geometry or multipolygon column")
-  }
-  ## Check: fill variable is numeric
-  if (!is.numeric(data[[fill]])) {
-    stop("fill must be type numeric")
   }
   ## Check: n variable is numeric
   if (!is.numeric(n)) {
